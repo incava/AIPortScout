0676c94809849eabef673deeb918741cde81ffe5
incava <68988975+incava@users.noreply.github.com>
2025-08-10 16:48:04 +0900

    [chore] Hilt 추가

---
+++ app/src/main/java/com/incava/aiportscout/AIPortScoutApplication.kt
@@ -0,0 +1,15 @@
+package com.incava.aiportscout
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ *  어플리케이션 클래스
+ */
+
+@HiltAndroidApp
+class AIPortScoutApplication : Application() {
+
+
+}
--- app/src/main/java/com/incava/aiportscout/MainActivity.kt
+++ app/src/main/java/com/incava/aiportscout/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.incava.aiportscout.ui.theme.AIPortScoutTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
